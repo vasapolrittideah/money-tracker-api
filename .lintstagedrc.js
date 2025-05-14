88995cf6967451a6d14ecf9aecfb261f3cb37340
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-05-15 00:24:11 +0700

    chore: update lint-staged configuration for go files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,7 @@
 export default {
-  "(services|shared)/**/*.go": ["go fmt", "go vet", "golangci-lint run"],
+  "(services|shared)/**/*.go": [
+    () => "go fmt ./...",
+    () => "go vet ./...",
+    () => "golangci-lint run",
+  ],
 };
