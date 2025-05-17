10ff3b5a85ba378e7ca7294a977f6965f1703bdc
vasapolrittideah <vasapol.rittideah@outlook.com>
2025-05-17 10:24:45 +0700

    chore: update golangci-lint to run on the whole package

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,12 @@
 export default {
-  "(services|shared)/**/*.go": (files) =>
-    files.flatMap((file) => [
-      `golangci-lint fmt ${file}`,
-      `golangci-lint run --fix ${file}`,
-    ]),
+  "(services|shared)/**/*.go": (files) => {
+    const directories = [
+      ...new Set(files.map((file) => file.replace(/\/[^/]+\.go$/, ""))),
+    ];
+
+    return directories.flatMap((dir) => [
+      `golangci-lint fmt ${dir}`,
+      `golangci-lint run --fix ${dir}`,
+    ]);
+  },
 };
